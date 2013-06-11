ba9f0299e7d9f9830d6de052e347d71e5ea58ada
Sean Dokko <dokko1230@gmail.com>
2013-06-11 12:51:32 -0700

    data is normalized and xml rewrite script is done. that took me waaay to long to figure out

--- normalize_xml.rb
+++ normalize_xml.rb
@@ -4,8 +4,23 @@ require 'nokogiri'
 puts "Normalizing the structure of the xml files"
 
 folder_path = File.dirname(__FILE__) + "/data"
-Dir.glob(folder_path + "/*").sort.each do |f|
-	puts xml
+Dir.glob(folder_path + "/*").sort.each do |path|
+    f = File.open(path, "r+")
+    doc = Nokogiri::XML(f)
+    doc.search(".//Root", ".//index").each do |node|
+        node.name=("root")
+        if node.element_children()[0].name() == "root"
+            node.children=(node.children().children())
+        end
+    end
+
+    f.close()
+    o = File.new(path, "w")
+    o.write(doc)
+    o.close()
+
 end
+
+#end
 
 puts "Normalizing complete."
